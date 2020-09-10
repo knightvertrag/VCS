d170dcc27f0bb24e97215f3d5430873e668ad6ee
knightvertrag <amiapu1@gmail.com>
2020-09-10 16:48:24 +0530

    Added basic commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string.h>
 
-int main()
+void init()
 {
-    std::cout << "Hello from Fenix!" << std::endl;
+    std::cout << "init used\n";
+}
+
+void add()
+{
+    std::cout << "add used\n";
+}
+
+void commit()
+{
+    std::cout << "commit used\n";
+}
+
+int main(int argc, char **argv)
+{
+    //std::cout << "Hello from Fenix!" << std::endl;
+    if (strcmp(argv[1], "init") == 0)
+    {
+        init();
+    }
+    if (strcmp(argv[1], "add") == 0)
+    {
+        add();
+    }
+    else if (strcmp(argv[1], "commit") == 0)
+    {
+        commit();
+    }
 }
