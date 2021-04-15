fd717314ea26dee75b9107ba02ecc9092dbc856b
knightvertrag <amiapu1@gmail.com>
2021-04-16 04:14:39 +0530

    Added a proto cat-file command

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
 #include "repository.h"
+#include "imperium_object.h"
 
 int main(int argc, char *argv[])
 {
+    imperium::Repository repo;
     if (strcmp(argv[1], "init") == 0)
     {
         if (argc == 3)
+        {
             try
             {
-                imperium::repo_create(argv[3]);
+                repo = imperium::repo_create(argv[3]);
             }
             catch (const std::exception &e)
             {
                 std::cerr << e.what() << '\n';
             }
+        }
         else
+        {
             try
             {
-                imperium::repo_create("./");
+                repo = imperium::repo_create("./");
             }
             catch (const std::exception &e)
             {
                 std::cerr << e.what() << '\n';
             }
+        }
+        
+    }
+    if(strcmp(argv[1], "cat-file") == 0)
+    {
+        //std::cout  << repo.impDir << "\n";
+        repo = imperium::repo_find();
+        imperium::object_read(repo, "0a868b7eab9caee8546556201c0d29f50440b0d8");
     }
     return 0;
 }
